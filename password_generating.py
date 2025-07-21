8dfaa962692d7d5f22019ca0fac12d4ecf7a0eca
Mustafaide20 <mustafaide20@gmail.com>
2025-07-22 01:12:01 +0200

    Update password_generating.py

--- password_generating.py
+++ password_generating.py
@@ -9,12 +9,13 @@ def password_generating(length) :
  password=''.join(random.choice(characters) for _ in range(length))
  return password
 
-if __name__=="__main__":
- user_length=input("enter passwrod length (max.12) \n")
-if user_length.isdigit():
-  length=int(user_length)
-else: 
- length=12
+if __name__ == "__main__":
+    user_length = input("enter password length (max.12) \n")
+    if user_length.isdigit():
+        length = int(user_length)
+    else:
+        length = 12
 
-generated=password_generating(length)
-print("ur password is  "+ generated )
+    generated = password_generating(length)
+    print("ur password is  " + generated)
+
